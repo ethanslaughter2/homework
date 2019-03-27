2fe6195640b7c06e7084250ca9cc63ec5d238f4a
Ethan <slaughter22222@gmail.com>
2019-03-27 16:49:14 -0500

    Added initial numbers file

---
+++ numbers.py
@@ -0,0 +1,18 @@
+def main():
+    try:
+        num1 = int(input('Enter the first number'))
+        pass
+    except valueError:
+        print('Make sure you entered in a number')
+
+    try:
+        num2 = int(input('Enter the second number'))
+        pass
+    except valueError:
+        print('Make sure you entered in a number')
+
+    calculate(num1, num2)
+
+def calculate(num1, num2):
+    if num1 > num2:
+        print('Num 1 is bigger than num 2')
