ddec0a5f947d9c6253136ec5387b653c27d17299
Ethan <slaughter22222@gmail.com>
2019-03-27 18:04:24 -0500

    fixing formatting for merge

--- numbers.py
+++ numbers.py
@@ -16,6 +16,7 @@ def main():
 def calculate(num1, num2):
     if num1 > num2:
         print('Number 1 is bigger than Number 2')
+        
     elif num1 < num2:
         print('Number 2 is bigger than Number 1')
     elif num1 = num2:
